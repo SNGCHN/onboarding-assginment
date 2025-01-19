c1efbf3daa8fc044912ecf6604beea985c181d4d
[SNGCHN] <[sngchn0312@naver.com]>
2025-01-20 03:57:06 +0900

    add: 로딩 스피너 추가

---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <div className="relative">
+        <div className="absolute inset-0 rounded-full border-4 border-transparent border-b-indigo-500 animate-spin"></div>
+        <div className="w-12 h-12 rounded-full border-4 border-gray-200"></div>
+      </div>
+    </div>
+  );
+}
