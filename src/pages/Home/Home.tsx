fe01755852c4cdad824b7be45e6d67cd98c851cd
[SNGCHN] <[sngchn0312@naver.com]>
2025-01-20 03:58:45 +0900

    fix: 이름 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Page>
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-2xl font-bold mb-6">Todo List</h1>
+        <h1 className="text-2xl font-bold mb-6">투두 리스트</h1>
         <TodoList />
       </div>
     </Page>
